06274aabdbdbefb72e9ab02eb0c5352676245294
Raghav <Raghavendra.Sai@mrcooper.com>
2019-02-13 22:20:21 +0530

    Updated read me and component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
 const styles = theme => ({
   button: {
@@ -12,17 +13,29 @@ const styles = theme => ({
   },
 });
 
-function ContainedButtons(props) {
+function calculate() {
+    axios.post('http://localhost:3000/api/pricing/calculatepricing')
+    .then(response => 
+        this.setState({response}
+        ))
+};
+// class StepSlider extends React.Component {
+//     state = {
+//         value: 200,
+//       };
+ function ContainedButtons(props) {
   const { classes } = props;
   return (
     <div>
       <Button variant="contained" color="primary" className={classes.button}
-      onClick={() => { alert('onClick'); }}>
+      onClick={() => { this.calculate(); }}>
         Calculate
       </Button>
     </div>
   );
 }
+}
+
 
 ContainedButtons.propTypes = {
   classes: PropTypes.object.isRequired,
